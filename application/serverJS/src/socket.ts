02c3b74ffed9f7556d4ccd84c39d46f449b89cce
trivankute <van.trinhlu02113010@hcmut.edu.vn>
2023-04-26 12:09:31 +0700

    fixed garbage truck path error because of forEach cannot break during its loop, must use basic for

--- application/serverJS/src/socket.ts
+++ application/serverJS/src/socket.ts
@@ -2,100 +2,99 @@ import { Server } from "socket.io";
 import prisma from "./utils/prisma";
 
 async function updateAllVehiclesPoints(socket: any) {
-    const vehicles = await prisma.vehicle.findMany({
-        where: {
-            state: "in progress"
+  const vehicles = await prisma.vehicle.findMany({
+    where: {
+      state: "in progress"
+    },
+    include: {
+      workers: {
+        select: {
+          id: true,
+          name: true,
         },
-        include: {
-            workers: {
-                select: {
-                    id: true,
-                    name: true,
-                },
-            },
-            task: {
-                select: {
-                    id: true,
-                    name: true,
-                    routes: true
-                },
-            },
+      },
+      task: {
+        select: {
+          id: true,
+          name: true,
+          routes: true
         },
+      },
+    },
+  });
+
+  if (vehicles.length == 0) {
+    // emit error
+    socket.emit("callVehiclesAfterUpdateAddressEvent", {
+      status: "error",
+      message: "No vehicles in progress"
     });
+    return
+  }
 
-    if (vehicles.length == 0) {
-        // emit error
-        socket.emit("callVehiclesAfterUpdateAddressEvent", {
-            status: "error",
-            message: "No vehicles in progress"
-        });
-        return
-    }
-
-    if(vehicles)
-    {
-        const vehicleUpdates = vehicles.map(async (vehicle: any) => {
-            if(vehicle && vehicle.task) {
-              const routes = vehicle.task.routes;
-              let pointIndex = vehicle.currentMovingPointIndex;
-              let currentPointLimit = 0;
-              routes.forEach(async (routeJson: any, index: number) => {
-                const route = JSON.parse(routeJson).geometry.coordinates;
-                if (pointIndex < route.length + currentPointLimit) {
-                  pointIndex++;
-                  if (index === routes.length - 1 && pointIndex === route.length + currentPointLimit) {
-                    return prisma.vehicle.update({
-                      where: {
-                        id: vehicle.id
-                      },
-                      data: {
-                        currentMovingPointIndex: 0
-                      }
-                    });
-                  }
-                  else {
-                    return prisma.vehicle.update({
-                      where: {
-                        id: vehicle.id
-                      },
-                      data: {
-                        currentMovingPointIndex: pointIndex
-                      }
-                    });
-                  }
-                }
-                else {
-                  currentPointLimit += route.length;
+  if (vehicles) {
+    const vehicleUpdates = vehicles.map(async (vehicle: any) => {
+      if (vehicle && vehicle.task) {
+        const routes = vehicle.task.routes;
+        let pointIndex = vehicle.currentMovingPointIndex;
+        let currentPointLimit = 0;
+        for (let i = 0; i < routes.length; i++) {
+          const route = JSON.parse(routes[i]).geometry.coordinates;
+          if (pointIndex < route.length + currentPointLimit) {
+            pointIndex++;
+            if (i === routes.length - 1 && pointIndex === route.length + currentPointLimit) {
+              return prisma.vehicle.update({
+                where: {
+                  id: vehicle.id
+                },
+                data: {
+                  currentMovingPointIndex: 0
                 }
               });
             }
-          });
-          
-          await Promise.all(vehicleUpdates);
-    }
-
-    socket.emit("callVehiclesAfterUpdateAddressEvent", {
-        status: "success"
+            else {
+              return prisma.vehicle.update({
+                where: {
+                  id: vehicle.id
+                },
+                data: {
+                  currentMovingPointIndex: pointIndex
+                }
+              });
+            }
+          }
+          else {
+            currentPointLimit += route.length;
+          }
+        }
+      }
     });
+
+    await Promise.all(vehicleUpdates);
+  }
+
+  socket.emit("callVehiclesAfterUpdateAddressEvent", {
+    status: "success"
+  });
 }
 
 
 let intervalId: NodeJS.Timeout;
 export default function configureSocket(server: any) {
-    const io = new Server(server, {
-        cors: {
-            origin: "*",
-        },
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+    },
+  });
+  io.on("connection", (socket) => {
+    console.log("a user connected");
+    if (intervalId) clearInterval(intervalId); // Clear previous interval if it exists
+    intervalId = setInterval(() => {
+      updateAllVehiclesPoints(socket);
+    }, 3000);
+    socket.on("disconnect", () => {
+      console.log("user disconnected");
     });
-    io.on("connection", (socket) => {
-        console.log("a user connected");
-        if (intervalId) clearInterval(intervalId); // Clear previous interval if it exists
-        intervalId = setInterval(() => {
-            updateAllVehiclesPoints(socket);
-        }, 3000);
-        socket.on("disconnect", () => {
-            console.log("user disconnected");
-        });
-    })
-    return io;
+  })
+  return io;
 }
